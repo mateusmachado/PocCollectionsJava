15899dac9f99a89d40a0c8f061bb5a243552fff1
mmachado.csp@gmail.com <mmachado.csp@gmail.com>
2016-06-13 15:34:44 -0300

    initial commit

---
+++ src/set/FindDups.java
@@ -0,0 +1,11 @@
+package set;
+
+import java.util.*;
+import java.util.stream.*;
+
+public class FindDups {
+	public static void main(String[] args) {
+		Set<String> distinctWords = Arrays.asList("a", "a", "A", "b", "d").stream().collect(Collectors.toSet());
+		System.out.println(distinctWords.size() + " distinct words: " + distinctWords);
+	}
+}
---
+++ test/PairTest.java
@@ -0,0 +1,9 @@
+import generic.Pair;
+
+public class PairTest {
+
+	public static void main(String[] args) {
+		Pair<Integer, String> worldCup = new Pair<Integer, String>(2010, "South Africa");
+		System.out.println("World cup" + worldCup.getFirst() + " in " + worldCup.getSecond());
+	}
+}
